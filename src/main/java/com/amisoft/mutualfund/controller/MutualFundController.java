a764179060b59ad7dba3548e6cec8ec58659fef5
theamistudy <the.amistudy@gmail.com>
2018-01-31 21:30:11 +0530

    Started with Spring Boot

--- src/main/java/com/amisoft/mutualfund/controller/MutualFundController.java
+++ src/main/java/com/amisoft/mutualfund/controller/MutualFundController.java
@@ -7,15 +7,12 @@ import com.amisoft.mutualfund.dao.MutualFundDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/mutualfund")
 public class MutualFundController {
 
